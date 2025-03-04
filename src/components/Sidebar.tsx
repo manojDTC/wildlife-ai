a7cbd8196941f64ca2ec0a9aecae07110b84c89a
manojDTC <manoj.ds@dtcinfotech.com>
2025-03-04 14:48:52 +0530

    revamped side bar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,25 @@
-import { ListItemIcon, MenuItem, MenuList, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { MenuItem, MenuList } from "@mui/material";
 import homeIcon from "../../src/assets/images/homeIcon.svg";
-import AlertsIcon from "../../src/assets/images/alerts-icon.svg";
+import alertsIcon from "../../src/assets/images/alerts-icon.svg";
+import { SidebarItemProps } from "../types/interface/SidebarItemProps.interface";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+
+  const [activeItem, setActiveItem] = useState<string>("");
+
+  const sideBarItems: SidebarItemProps[] = [
+    { title: "Home", icon: homeIcon, link: "" },
+    { title: "Alerts", icon: alertsIcon, link: "alerts" },
+  ];
+
+  const handleClick = (link: string) => {
+    navigate(`/${link}`);
+    setActiveItem(link);
+  };
+
   return (
     <MenuList
       sx={{
@@ -16,21 +32,29 @@ const Sidebar = () => {
         padding: "0",
       }}
     >
-      <Link to="/">
-        <MenuItem
-          sx={{
-            justifyContent: "center",
-            padding: "15px 0",
-          }}
-        >
-          <img src={homeIcon} alt="homeIcon"></img>
-        </MenuItem>
-      </Link>
-      <Link to="/alerts">
-        <MenuItem sx={{ justifyContent: "center", padding: "15px 0" }}>
-          <img src={AlertsIcon} alt="ticketsIcon"></img>
-        </MenuItem>
-      </Link>
+      {sideBarItems?.map((item, index) => {
+        return (
+          <MenuItem
+            onClick={() => handleClick(item.link)}
+            key={index}
+            sx={{
+              justifyContent: "center",
+              padding: "15px 0",
+              backgroundColor:
+                item.link === `${activeItem}`
+                  ? "rgb(116, 96, 171)"
+                  : "transparent",
+              borderLeft:
+                item.link === `${activeItem}`
+                  ? "5px rgb(169, 167, 174)"
+                  : "5px solid transparent",
+              "&:hover": { backgroundColor: "transparent" },
+            }}
+          >
+            <img src={item.icon} alt="Icon"></img>
+          </MenuItem>
+        );
+      })}
     </MenuList>
   );
 };
